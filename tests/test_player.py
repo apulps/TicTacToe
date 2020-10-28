f90b05de9f5943da466803f549bdc471d49b2820
apulps <apinedaminon@gmail.com>
2020-10-28 19:23:28 +0100

    Add Player test #1

---
+++ tests/test_player.py
@@ -0,0 +1,32 @@
+import unittest
+
+from game.tictactoe import Player, Color, Piece
+
+
+
+class TestPlayer(unittest.TestCase):
+    def setUp(self):
+        self.player1 = Player("Bob", Color.WHITE)
+        self.player2 = Player("Steve", Color.BLACK)
+    
+    def test_constructor(self):
+        self.assertIsInstance(self.player1, Player)
+        self.assertIsInstance(self.player2, Player)
+    
+    def test_get_color(self):
+        self.assertIs(self.player1.get_color(), Color.WHITE)
+        self.assertIs(self.player2.get_color(), Color.BLACK)
+
+    def test_get_name(self):
+        self.assertEqual(self.player1.get_name(), "Bob")
+        self.assertEqual(self.player2.get_name(), "Steve")
+
+    def test_create_piece(self):
+        piece1 = self.player1.create_piece()
+        piece2 = self.player2.create_piece()
+
+        self.assertIsInstance(piece1, Piece)
+        self.assertIsInstance(piece2, Piece)
+
+        self.assertIs(self.player1.get_color(), piece1.get_color())
+        self.assertIs(self.player2.get_color(), piece2.get_color())
